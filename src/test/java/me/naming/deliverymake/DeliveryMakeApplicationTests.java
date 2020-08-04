c357b72badf9c258efc040492e5067f0f3726b98
tjdrnr0557 <tjdrnr05571@naver.com>
2020-08-04 20:41:58 +0900

    메이븐 빌드툴을 이용해 스프링 부트 프로젝트 생성

---
+++ src/test/java/me/naming/deliverymake/DeliveryMakeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.naming.deliverymake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryMakeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
