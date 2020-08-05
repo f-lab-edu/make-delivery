125c8ad97b4f97e621785eaccda6e8a0b60c2ded
tjdrnr0557 <tjdrnr05571@naver.com>
2020-08-05 21:58:34 +0900

    패키지 작명 규칙에 따라 groupId 변경

--- src/main/java/com/flab/makedel/DeliveryMakeApplication.java
+++ src/main/java/com/flab/makedel/DeliveryMakeApplication.java
@@ -1,4 +1,4 @@
-package com.makedel.deliverymake;
+package com.flab.makedel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/flab/makedel/DeliveryMakeApplicationTests.java
+++ src/test/java/com/flab/makedel/DeliveryMakeApplicationTests.java
@@ -1,4 +1,4 @@
-package com.makedel.deliverymake;
+package com.flab.makedel;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
