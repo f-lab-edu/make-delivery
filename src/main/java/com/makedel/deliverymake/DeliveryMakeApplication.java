ceb6c071da797e5dccc4a2c304e5112bc822dc45
tjdrnr0557 <tjdrnr05571@naver.com>
2020-08-05 16:34:50 +0900

    groupId 변경

--- src/main/java/com/makedel/deliverymake/DeliveryMakeApplication.java
+++ src/main/java/com/makedel/deliverymake/DeliveryMakeApplication.java
@@ -1,4 +1,4 @@
-package me.naming.deliverymake;
+package com.makedel.deliverymake;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/makedel/deliverymake/DeliveryMakeApplicationTests.java
+++ src/test/java/com/makedel/deliverymake/DeliveryMakeApplicationTests.java
@@ -1,4 +1,4 @@
-package me.naming.deliverymake;
+package com.makedel.deliverymake;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
