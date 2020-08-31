a514cac79b77f24b071518af8415198b22021792
tjdrnr0557 <tjdrnr05571@naver.com>
2020-08-31 14:36:30 +0900

    [#6] 코드리뷰 반영 : 주석 변경과 @EnableAspectJAutoProxy 생략

--- src/main/java/com/flab/makedel/DeliveryMakeApplication.java
+++ src/main/java/com/flab/makedel/DeliveryMakeApplication.java
@@ -2,7 +2,6 @@ package com.flab.makedel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /*
@@ -12,13 +11,16 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
     springSessionRepositoryFilter 필터는 HttpSession을 스프링세션에 의해 지원되는 커스템 구현체로 바꿔줍니다.
     이 어노테이션이 붙은 곳에서는 레디스가 스프링 세션을 지원합니다.
 
-    @EnableAspectJAutoProxy: AspectJ 코드 스타일로 @Aspect 어노테이션을 사용가능하게 해줍니다.
-    또한 AOP를 RTW인 다이나믹 프록시 방식으로 사용가능하게 해줍니다.
+    @EnableAspectJAutoProxy:
+    AOP를 RTW인 다이나믹 프록시 방식으로 사용가능하게 해줍니다.
+    @SpringbootAplication 어노테이션 안에 @EnableAutoConfiguration이 있고
+    이 어노테이션은 스프링부트가 클래스패스에서 찾은 빈들을 설정하게 해줍니다.
+    따라서 스프링이 @Aspect를 보고 프록시방식으로 사용하게 해주기 때문에
+    @EnableAspectJAutoProxy가 생략가능합니다.
  */
 
 @SpringBootApplication
 @EnableRedisHttpSession
-@EnableAspectJAutoProxy
 public class DeliveryMakeApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/flab/makedel/aop/LoginCheck.java
+++ src/main/java/com/flab/makedel/aop/LoginCheck.java
@@ -8,6 +8,10 @@ import java.lang.annotation.Target;
 /*
  *   Run-Time Weaving을 사용하는 다이나믹 프록시 방식의 SPRING AOP를 사용할 것이기 때문에
  *   Retention은 Runtime까지로 지정해줬습니다.
+ *
+ *   프록시 객체는 런타임에 생성하지만
+ *   런타임에 그 프록시를 생성할 타겟 빈을 찾을때 클래스정보(바이트 코드)를 참고하기 때문에
+ *   런타임까지 해당 어노테이션을 유지할 필요는 없습니다. Retention을 클래스까지로해도 똑같이 AOP적용이 가능합니다.
  * */
 
 @Retention(RetentionPolicy.RUNTIME)
