b3a9e778fcd40f87a0d1cda62e1c88748ffdbac1
tjdrnr0557 <tjdrnr05571@naver.com>
2020-10-16 02:17:44 +0900

    오류 수정

--- src/main/java/com/flab/makedel/controller/OrderController.java
+++ src/main/java/com/flab/makedel/controller/OrderController.java
@@ -13,7 +13,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,13 +31,14 @@ public class OrderController {
         return orderReceipt;
     }
 
-//    @GetMapping
-//    @LoginCheck(userLevel = UserLevel.USER)
-//    public OrderDetailDTO loadOrder(long orderId) {
-//        return orderService.getOrderInfoByOrderId(orderId);
-//    }
+    @GetMapping("/{orderId}")
+    @LoginCheck(userLevel = UserLevel.USER)
+    public OrderDetailDTO loadOrder(@PathVariable long orderId) {
+        return orderService.getOrderInfoByOrderId(orderId);
+    }
 
     @GetMapping
+    @LoginCheck(userLevel = UserLevel.OWNER)
     public List<OrderStoreDetailDTO> loadStoreOrder(@PathVariable long storeId) {
         return orderService.getStoreOrderInfoByStoreId(storeId);
     }
