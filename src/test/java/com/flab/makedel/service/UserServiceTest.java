f3565c4909704593bf64c6a6e94dc3a992fa0b18
tjdrnr0557 <tjdrnr05571@naver.com>
2020-11-05 21:53:14 +0900

    UserService 테스트 작성

--- src/test/java/com/flab/makedel/service/UserServiceTest.java
+++ src/test/java/com/flab/makedel/service/UserServiceTest.java
@@ -3,10 +3,15 @@ package com.flab.makedel.service;
 import static org.mockito.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import com.flab.makedel.annotation.LoginCheck.UserLevel;
 import com.flab.makedel.dto.UserDTO;
 import com.flab.makedel.mapper.UserMapper;
+import com.flab.makedel.utils.PasswordEncrypter;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -22,23 +27,69 @@ class UserServiceTest {
     @InjectMocks
     UserService userService;
 
-    @Test
-    void signUpTestWhenSuccess() {
+    UserDTO user;
 
-        UserDTO user = UserDTO.builder()
+    @BeforeEach
+    public void makeUser() {
+        user = UserDTO.builder()
             .id("user")
-            .password("123")
+            .password(PasswordEncrypter.encrypt("123"))
             .email("tjdrnr05571@naver.com")
             .name("이성국")
             .phone("010-1234-1234")
             .address("서울시")
             .level(UserLevel.USER)
             .build();
+    }
 
+    @Test
+    public void signUpTestWhenSuccess() {
         userService.signUp(user);
 
         verify(userMapper).insertUser(any(UserDTO.class));
-
     }
+
+    @Test
+    public void isExistsIdTestWhenDuplicatedId() {
+        when(userMapper.isExistsId(user.getId())).thenReturn(true);
+
+        assertEquals(userService.isExistsId(user.getId()), true);
+
+        verify(userMapper).isExistsId(user.getId());
+    }
+
+    @Test
+    public void isExistsIdTestWhenNotDuplicatedId() {
+        when(userMapper.isExistsId(user.getId())).thenReturn(false);
+
+        assertEquals(userService.isExistsId(user.getId()), false);
+
+        verify(userMapper).isExistsId(user.getId());
+    }
+
+    @Test
+    public void deleteUserTestWhenSuccess() {
+        userService.deleteUser(user.getId());
+
+        verify(userMapper).deleteUser(user.getId());
+    }
+
+    @Test
+    public void changeUserPasswordTestWhenSuccess() {
+        userService.changeUserPassword(user.getId(), "123");
+
+        verify(userMapper).updateUserPassword(any(String.class), any(String.class));
+    }
+
+    @Test
+    public void findUserByIdAndPasswordTestWhenSuccess() {
+        when(userMapper.selectUserById(user.getId())).thenReturn(user);
+
+        assertEquals(userService.findUserByIdAndPassword(user.getId(), "123"),
+            Optional.ofNullable(user));
+
+        verify(userMapper).selectUserById(user.getId());
+    }
+
 
 }
