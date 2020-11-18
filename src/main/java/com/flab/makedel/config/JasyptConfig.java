047882e6ede913dd529240c0efbe90cfd43043a7
tjdrnr0557 <tjdrnr05571@naver.com>
2020-11-18 13:14:12 +0900

    jasypt를 통한 암호화

---
+++ src/main/java/com/flab/makedel/config/JasyptConfig.java
@@ -0,0 +1,23 @@
+package com.flab.makedel.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+    @Bean
+    public StringEncryptor jasyptStringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword("sunggook");
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}
