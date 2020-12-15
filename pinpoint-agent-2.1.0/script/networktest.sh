fd1e3e5e341a0768c4549a98026e57043d242c05
tjdrnr0557 <tjdrnr05571@naver.com>
2020-12-15 20:26:11 +0900

    d

---
+++ pinpoint-agent-2.1.0/script/networktest.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+WORKING_DIR=$(dirname "$0")
+cd -P "$WORKING_DIR"/.. > /dev/null
+
+CL_PATH=$(find ./tools -name '*.jar' | tr "\n" :)
+echo "CLASSPATH=$CL_PATH"
+
+java -cp $CL_PATH com.navercorp.pinpoint.tools.NetworkAvailabilityChecker ./pinpoint.config
