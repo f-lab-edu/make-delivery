0d6867d88ae9b6d2a25055f6ceef2e340e02f07e
tjdrnr0557 <tjdrnr05571@naver.com>
2020-12-18 02:02:16 +0900

    주문 승인 테스트 추가

--- src/main/java/com/flab/makedel/dto/StoreInfoDTO.java
+++ src/main/java/com/flab/makedel/dto/StoreInfoDTO.java
@@ -3,9 +3,11 @@ package com.flab.makedel.dto;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class StoreInfoDTO {
 
     private final Long storeId;
--- src/test/java/com/flab/makedel/service/StoreServiceTest.java
+++ src/test/java/com/flab/makedel/service/StoreServiceTest.java
@@ -7,13 +7,21 @@ import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.mockito.Matchers.*;
 
+import com.flab.makedel.dao.DeliveryDAO;
+import com.flab.makedel.dto.CartItemDTO;
 import com.flab.makedel.dto.OrderDTO.OrderStatus;
 import com.flab.makedel.dto.OrderReceiptDTO;
+import com.flab.makedel.dto.PushMessageDTO;
 import com.flab.makedel.dto.StoreDTO;
+import com.flab.makedel.dto.StoreInfoDTO;
+import com.flab.makedel.dto.UserInfoDTO;
 import com.flab.makedel.mapper.OrderMapper;
 import com.flab.makedel.mapper.StoreMapper;
+import com.google.firebase.auth.UserInfo;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -31,6 +39,12 @@ public class StoreServiceTest {
 
     @Mock
     OrderMapper orderMapper;
+
+    @Mock
+    DeliveryService deliveryService;
+
+    @Mock
+    RiderService riderService;
 
     @InjectMocks
     StoreService storeService;
@@ -128,8 +142,47 @@ public class StoreServiceTest {
     @Test
     @DisplayName("사장이 주문을 승인하는데 성공합니다")
     public void approveOrderTestSuccess() {
+
+        UserInfoDTO userInfo = UserInfoDTO.builder()
+            .id("사용자1")
+            .name("이성국")
+            .phone("010-1212-1212")
+            .address("경기도 고양시 일산동")
+            .build();
+
+        StoreInfoDTO storeInfo = StoreInfoDTO.builder()
+            .storeId(1L)
+            .name("bbq치킨")
+            .phone("010-1234-1234")
+            .address("경기도 고양시 일산동")
+            .build();
+
+        List<CartItemDTO> cartList = new ArrayList<>();
+
+        OrderReceiptDTO orderReceiptDTO = OrderReceiptDTO.builder()
+            .orderId(1L)
+            .orderStatus("COMPLETE_ORDER")
+            .userInfo(userInfo)
+            .storeInfo(storeInfo)
+            .totalPrice(123000L)
+            .cartList(cartList)
+            .build();
+
         doNothing().when(orderMapper).approveOrder(anyLong(), any(OrderStatus.class));
-        when(orderMapper.selectOrderReceipt(anyLong())).thenReturn(any(OrderReceiptDTO.class));
+        when(orderMapper.selectOrderReceipt(anyLong())).thenReturn(orderReceiptDTO);
+        doNothing().when(deliveryService)
+            .registerStandbyOrderWhenOrderApprove(anyLong(), any(OrderReceiptDTO.class));
+        doNothing().when(riderService)
+            .sendMessageToStandbyRidersInSameArea(any(String.class), any(PushMessageDTO.class));
+
+        storeService.approveOrder(1);
+
+        verify(orderMapper).approveOrder(anyLong(), any(OrderStatus.class));
+        verify(orderMapper).selectOrderReceipt(anyLong());
+        verify(deliveryService)
+            .registerStandbyOrderWhenOrderApprove(anyLong(), any(OrderReceiptDTO.class));
+        verify(riderService)
+            .sendMessageToStandbyRidersInSameArea(any(String.class), any(PushMessageDTO.class));
     }
 
 }
