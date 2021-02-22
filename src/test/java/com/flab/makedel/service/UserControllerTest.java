ee1ac3ebac92fc02309248ac1fae0fd25004cd35
tjdrnr0557 <tjdrnr05571@naver.com>
2021-02-22 16:02:38 +0900

    UserController 내 로그인 테스트

--- src/test/java/com/flab/makedel/service/UserControllerTest.java
+++ src/test/java/com/flab/makedel/service/UserControllerTest.java
@@ -2,6 +2,8 @@ package com.flab.makedel.service;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -10,7 +12,11 @@ import com.flab.makedel.annotation.LoginCheck.UserLevel;
 import com.flab.makedel.config.RedisConfig;
 import com.flab.makedel.controller.UserController;
 import com.flab.makedel.dto.UserDTO;
+import com.flab.makedel.mapper.UserMapper;
+import com.flab.makedel.utils.PasswordEncrypter;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -48,6 +54,7 @@ class UserControllerTest {
 
 
     @Test
+    @DisplayName("/users로 POST메소드를 보내 회원가입을 요청하는데 성공한다")
     void signUpTest() throws Exception {
         MultiValueMap<String, String> paramMap = new LinkedMultiValueMap<>();
         paramMap.add("id", "here");
@@ -66,4 +73,80 @@ class UserControllerTest {
 
         verify(userService).signUp(any(UserDTO.class));
     }
+
+    @Test
+    @DisplayName("/{userId}/exists에 Get메소드로 중복된 아이디인지 체크하고 중복이 아니라면 200 상태코드를 반환한다")
+    void checkUniqueIdTest() throws Exception {
+        when(userService.isExistsId("newid1")).thenReturn(true);
+
+        mockMvc.perform(
+            get("/users/newid1/exists")
+        )
+            .andExpect(status().isOk());
+
+        verify(userService).isExistsId(any(String.class));
+    }
+
+    @Test
+    @DisplayName("/{userId}/exists에 Get메소드로 중복된 아이디인지 체크하고 중복이 아니라면 200 상태코드를 반환한다")
+    void checkUniqueIdTest_중복() throws Exception {
+        when(userService.isExistsId("newid1")).thenReturn(false);
+
+        mockMvc.perform(
+            get("/users/newid1/exists")
+        )
+            .andExpect(status().is4xxClientError());
+
+        verify(userService).isExistsId(any(String.class));
+
+    }
+
+    @Test
+    @DisplayName("/login에 Post 메소드로 로그인을 하는데 성공한다")
+    void loginTest() throws Exception {
+        UserDTO userDTO = UserDTO.builder()
+            .id("id")
+            .password(PasswordEncrypter.encrypt("pass"))
+            .name("name")
+            .email("email")
+            .phone("phone")
+            .address("ad")
+            .level(UserLevel.USER)
+            .build();
+
+        MultiValueMap<String, String> paramMap = new LinkedMultiValueMap<>();
+        paramMap.add("id", userDTO.getId());
+        paramMap.add("password", "pass");
+
+        Optional<UserDTO> user = Optional.ofNullable(userDTO);
+        when(userService.findUserByIdAndPassword(userDTO.getId(), "pass"))
+            .thenReturn(user);
+
+        mockMvc.perform(post("/users/login")
+            .contentType(MediaType.APPLICATION_JSON)
+            .params(paramMap))
+            .andExpect(status().isOk());
+
+        verify(userService).findUserByIdAndPassword(any(String.class), any(String.class));
+    }
+
+    @Test
+    @DisplayName("/login에 Post 메소드로 잘못된 비밀번호로 로그인을 하는데 실패한다")
+    void loginTest_실패() throws Exception {
+
+        MultiValueMap<String, String> paramMap = new LinkedMultiValueMap<>();
+        paramMap.add("id", "sameid");
+        paramMap.add("password", "wrong");
+
+        when(userService.findUserByIdAndPassword("sameid", "wrong"))
+            .thenReturn(Optional.empty());
+
+        mockMvc.perform(post("/users/login")
+            .contentType(MediaType.APPLICATION_JSON)
+            .params(paramMap))
+            .andExpect(status().is4xxClientError());
+
+        verify(userService).findUserByIdAndPassword(any(String.class), any(String.class));
+    }
+
 }
